90c6b221767db271d80e30419ecd1fa1acdc4e7c
DrOtter3000 <info@darknet-online.com>
2025-11-15 12:43:22 +0100

    make gravity more relateable

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -8,7 +8,11 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y < 0:
+			velocity += get_gravity() * delta * 0.8
+		else:
+			velocity += get_gravity() * delta
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
