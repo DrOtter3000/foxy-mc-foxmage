b3786ae551ef0465238e9cfec87f968fc1abc07a
DrOtter3000 <info@darknet-online.com>
2025-11-16 16:37:59 +0100

    make enemy flip sprite

--- Characters/Enemies/enemy_template.gd
+++ Characters/Enemies/enemy_template.gd
@@ -3,15 +3,18 @@ extends CharacterBody2D
 
 @export var health := 2.0
 @export var speed := 100
-@export var direction := 1 # 1 == left, -1 == right
+@export var dir := -1 # 1 == left, -1 == right
+@onready var sprite: Sprite2D = $Sprite2D
 
+func _ready() -> void:
+	print(dir)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	velocity.x = speed * direction
+	
+	velocity.x = speed * dir
 	move_and_slide()
 
 func take_damage(damage) -> void:
@@ -23,4 +26,5 @@ func die() -> void:
 	queue_free()
 
 func switch_direction(_body) -> void:
-	direction *= -1
+	dir *= -1
+	sprite.scale.x *= -1
